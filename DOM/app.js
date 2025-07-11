db057fa19f91180c02330280b1399ce1f240faf4
Giohandelcas <giohandelcas@gmail.com>
2025-07-11 01:14:47 -0500

    DOM manage

---
+++ DOM/app.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById("cambiarColor")
+const caja = document.getElementById("caja")
+
+boton.addEventListener("click", () => {
+    caja.style.background = "blue"
+})
